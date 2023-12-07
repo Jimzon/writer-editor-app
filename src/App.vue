d358196f158879807cbc19cf2fba617a5b504147
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-12-08 00:05:34 +0800

    lang page header  and dashboard header condition

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,48 @@
-
 <script setup>
-import { ref, computed } from 'vue'
-// import Navbar from './components/Navbar.vue'
-import Login from './pages/Login.vue'
-import Signup from './pages/Signup.vue'
-import Home from './pages/home/Home.vue'
-import NavbarDashboard from './pages/dashboard/NavbarDashboard.vue'
-import HomeDashboardVue from './pages/dashboard/HomeDashboard.vue'
-import WriterForm from './pages/WriterForm.vue'
+import { ref, computed } from "vue";
+import Navbar from "./components/Navbar.vue";
+import Login from "./pages/Login.vue";
+import Signup from "./pages/Signup.vue";
+import Home from "./pages/home/Home.vue";
+import NavbarDashboard from "./pages/dashboard/NavbarDashboard.vue";
+import HomeDashboardVue from "./pages/dashboard/HomeDashboard.vue";
+import WriterForm from "./pages/WriterForm.vue";
 
 const routes = {
-  '/': Home,
-  '/login': Login,
-  '/sign-up': Signup,
-  '/dashboard': HomeDashboardVue,
-  '/writer-form': WriterForm,
-}
+  "/": Home,
+  "/login": Login,
+  "/sign-up": Signup,
+  "/dashboard": HomeDashboardVue,
+  "/writer-form": WriterForm,
+};
 
-const currentPath = ref(window.location.hash)
+const currentPath = ref(window.location.hash);
 
-window.addEventListener('hashchange', () => {
-  currentPath.value = window.location.hash
-})
+window.addEventListener("hashchange", () => {
+  currentPath.value = window.location.hash;
+});
 
 const currentView = computed(() => {
-  return routes[currentPath.value.slice(1) || '/'] || NotFound
-})
+  return routes[currentPath.value.slice(1) || "/"] || NotFound;
+});
+
+const isDashboard = computed(() => {
+  console.log(currentPath.value);
+  return (
+    currentPath.value !== "/" &&
+    currentPath.value !== "/#" &&
+    currentPath.value !== "#/login" &&
+    currentPath.value !== "#/sign-up"
+  );
+});
 </script>
 
 <template>
-    <header>
-      <!-- <Navbar /> -->
-      <NavbarDashboard />
-    </header>
-
-    
-
-      <main>  
-        <component :is="currentView" />
-      </main>
+  <header>
+    <NavbarDashboard v-if="isDashboard" />
+    <Navbar v-if="!isDashboard" />
+  </header>
+  <main>
+    <component :is="currentView" />
+  </main>
 </template>
-
