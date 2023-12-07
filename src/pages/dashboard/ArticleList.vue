724a95e715ba7cc5144c6cd21d7250026d14e733
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-12-08 00:55:37 +0800

    modified articles list and navbar dashboard

--- src/pages/dashboard/ArticleList.vue
+++ src/pages/dashboard/ArticleList.vue
@@ -1,18 +1,20 @@
 <script setup>
-import ForEditList from './ForEditList.vue'
-import PublishedList from './PublishedList.vue';
+import ForEditList from "./ForEditList.vue";
+import PublishedList from "./PublishedList.vue";
 </script>
 
 <template>
-    <div class="py-24 justify-center mx-auto items-center max-w-4xl p-3">
-        <div class="flex justify-between items-center">
-          <h2 class="font-semibold text-xl">Articles List</h2>
-            <a href="#/writer-form" class="bg-sky-800 flex text-white font-normal py-2 px-4 items-center text-md rounded-full">
-                 <span class="text-xl pr-2">+</span>  Create Article
-            </a>
-        </div>
-        <ForEditList />
-        <PublishedList />
+  <div class="py-24 justify-center mx-auto items-center max-w-4xl p-3">
+    <div class="flex justify-between items-center">
+      <h2 class="font-semibold text-xl">Articles List</h2>
+      <a
+        href="#/writer-form"
+        class="bg-sky-800 flex text-white font-normal py-1.5 px-3.5 items-center text-md rounded-full"
+      >
+        <span class="text-md pr-2">+</span>Create Article
+      </a>
     </div>
-
+    <ForEditList />
+    <PublishedList />
+  </div>
 </template>
--- src/pages/dashboard/NavbarDashboard.vue
+++ src/pages/dashboard/NavbarDashboard.vue
@@ -1,37 +1,45 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-
-<header class="py-4 drop-shadow-lg box-shadow-lg shadow-lg">
-    <div class=" flex gap-4 justify-between items-center max-w-6xl mx-auto p-3">
-       <div class="flex gap-16">
+  <header class="py-4 drop-shadow-lg box-shadow-lg shadow-lg">
+    <div class="flex gap-4 justify-between items-center max-w-6xl mx-auto p-3">
+      <div class="flex gap-16">
         <a href="#/">
-            <img alt="Vue logo" class="w-24" src="/src/assets/archintel-logo.gif"  />
+          <img
+            alt="Vue logo"
+            class="w-24"
+            src="/src/assets/archintel-logo.gif"
+          />
         </a>
-    
-        <ul class="flex gap-8">
-          <a href="" class="font-semibold">Dashboard</a>
-          <a href="">All Media</a>
-        </ul>
 
-       </div>
-        
-    <div class="flex gap-6 items-center cursor-pointer">
+        <ul class="flex gap-8">
+          <a
+            href="#/dashboard"
+            class="hover:underline duration-500 transition-all"
+            >Dashboard</a
+          >
+          <a
+            href="#/articles-table"
+            class="hover:underline duration-500 transition-all"
+            >All Media</a
+          >
+        </ul>
+      </div>
+
+      <div class="flex gap-6 items-center cursor-pointer">
         <div class="block">
-            <h3 class="text-slate-700 font-semibold">
-             Leonora Hale
-            </h3>
-            <h4>Writer</h4>  
+          <h3 class="text-slate-700 font-semibold">Leonora Hale</h3>
+          <h4>Writer</h4>
         </div>
         <a href="#/login" class="rounded-full h-12 w-12">
-         <img class="w-16 h-12 rounded-full mx-auto" src="/src/assets/sample-img.jpg" alt="profile-image" srcset="">
+          <img
+            class="w-16 h-12 rounded-full mx-auto"
+            src="/src/assets/sample-img.jpg"
+            alt="profile-image"
+            srcset=""
+          />
         </a>
-
-       </div>
+      </div>
     </div>
   </header>
-
 </template>
-
